fbf8a85090ebb78909e3ddd0eb2f0a4a9e80157f
LUIS ARCE <luisarce5@gmail.com>
2015-11-05 12:25:33 -0500

    added Model comment.rb; Partial Form  _comment.html.erb related to render partial in show.html.erb; created and migrated table for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105010826) do
+ActiveRecord::Schema.define(version: 20151105170122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +26,15 @@ ActiveRecord::Schema.define(version: 20151105010826) do
   create_table "bloggers", force: :cascade do |t|
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  add_foreign_key "comments", "articles"
 end
